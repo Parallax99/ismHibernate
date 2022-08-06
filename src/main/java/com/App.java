6df082853a29ba2acd97d714971b5c86d1180f09
Parallax99 <abhishekpillai6@gmail.com>
2022-08-06 18:04:14 +0530

    hibernate-demo

---
+++ src/main/java/com/App.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Configuration cfg = new Configuration().configure("hibernate-config.xml");
+
+		// use to create session
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		// use to execute query
+		Session session = factory.openSession();//every time open
+    }
+}
